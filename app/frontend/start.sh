8823d947ebe6062a66f25abc25fa597b908dd922
Mahesh Kotakonda <mahesh04815@gmail.com>
2026-02-05 19:04:26 +0530

    Update start.sh

--- app/frontend/start.sh
+++ app/frontend/start.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "Preparing nginx log directories..."
-mkdir -p /var/log/nginx
-
 echo "Generating env.js..."
 cat <<EOF > /usr/share/nginx/html/env.js
 const FRONTEND_VERSION = "${FRONTEND_VERSION:-unknown}";
@@ -11,4 +8,4 @@ const ENVIRONMENT = "${ENVIRONMENT:-unknown}";
 EOF
 
 echo "Starting nginx..."
-exec nginx -g 'daemon off; access_log /var/log/nginx/access.log; error_log /var/log/nginx/error.log warn;'
+exec nginx -g 'daemon off; error_log /dev/stderr warn;'
